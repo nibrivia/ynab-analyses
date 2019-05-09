09671b403a1a857ab2a5b6e98671507fe2f6eb5c
Olivia Brode-Roger <nibr@mit.edu>
2019-05-09 13:53:01 -0400

    Cleaner code

--- ynab.py
+++ ynab.py
@@ -3,19 +3,25 @@ import json
 
 # TODO implement delta transactions
 
-def import_txs(filename):
+def import_txs(filename = "transactions.json"):
     with open(filename) as file:
         tx_response = json.load(file)
     return tx_response["data"]["transactions"]
 
+def txs_to_csv(transactions, filename = "transactions.csv"):
+    with open(filename, "w") as csv_file:
+        writer = csv.writer(csv_file)
+        keys = list(transactions[0].keys())
+
+        # Headers
+        writer.writerow(keys)
+        # Rows
+        for t in transactions:
+            writer.writerow([t[k] for k in keys])
+
+
 if __name__ == "__main__":
     transactions = import_txs("transactions.json")
-
-    with open("transactions.csv", "w") as csv_file:
-        csv_writer = csv.writer(csv_file)
-        keys = list(transactions[0].keys())
-        for t in transactions:
-            csv_writer.writerow([t[k] for k in keys])
-
+    txs_to_csv(transactions = transactions, filename = "transactions.csv")
     print("done")
 
